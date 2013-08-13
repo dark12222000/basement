9b60c6e02fb507fc52fe37dda9d6e77b14b8b173
Joe L. Wroten <sharpshark28@gmail.com>
2013-08-13 12:59:52 -0400

    Creating a Room and Signing in as admin
    Clientside, check console logs to confirm. No error checking yet.

---
+++ client/js/app.js
@@ -0,0 +1,33 @@
+$(document).foundation();
+
+var socket = io.connect('http://bs1.adventurestory.net:3000');
+
+socket.on('connected', function(event){
+	console.log('Connected successfully to socket', event);
+	
+	var room = new Object();
+	var user = new Object();
+	
+	$('#createRoom').foundation('reveal', 'open');
+	
+	/* UI Binds */
+	$('.createRoom').on('click', function(){
+		socket.emit('createRoom');
+	});
+	
+	$('form.login').submit(function(){
+		socket.emit('registerClient', { name: $(this).find('input[name=name]').val(), roomID: room.id });
+		$(this).closest('form').foundation('reveal', 'close');
+		return false;
+	});
+	
+	/* Event Binds */
+	socket.on('roomCreated', function(response){
+		console.log('roomCreated: ', response);
+		room.id = response.roomID;
+	});
+	
+	socket.on('clientRegistered', function(response){
+		console.log('clientRegistered: ', response);
+	});
+});
