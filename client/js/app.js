1b0253174e5330cdaacb198cf1b07b41be99b435
sharpshark28 <sharpshark28@gmail.com>
2013-08-13 21:18:13 -0400

    Connected to room visual conf, url field
    Visually lets you see what to link others to.

--- client/js/app.js
+++ client/js/app.js
@@ -32,8 +32,16 @@ socket.on('connected', function(event){
 		console.log('clientRegistered: ', response);
 		user.id = response.userID;
 		user.isAdmin = response.admin;
+		$('#socket-connected').fadeIn()
+			.find('#socket-url').val(document.location.pathname + '#' + room.id)
+			.on('click', function(){ this.select(); }
+		);
 		socket.emit('getClients', { sender: user.ID, roomID: room.id });
 	});
+	
+	window.onbeforeunload = function(){
+		//return "test"; // Unregister the user if we can
+	}
 	
 	socket.on('sendClients', function(response){
 		console.log('sendClients: ', response);
@@ -42,5 +50,6 @@ socket.on('connected', function(event){
 		$.each(response.clients, function(index, user) {
 			$('#socket-users').append('<li>'+user);
 		});
+		$('#socket-room').text('You are connected to room ' + room.id);
 	});
 });
