f1a47ea9000c61ecf2a7248f3a0d8790903963dc
sharpshark28 <sharpshark28@gmail.com>
2013-08-24 17:20:26 -0400

    Admin and User styles consistent
    Users list and chat room has matching user type colors of regular user
    and admin user. (Blue and green respectively)

--- client/js/app.js
+++ client/js/app.js
@@ -57,7 +57,6 @@ socket.on('connected', function(event){
 	
 	socket.on('sendClients', function(response){
 		console.log('sendClients: ', response);
-		console.log('Connected Clients are: ', response.clients);
 		$('#socket-users').html('');
 		$.each(response.clients, function(index, user) {
 			$('#socket-users').append('<li>'+user);
@@ -70,15 +69,15 @@ socket.on('connected', function(event){
 	});
 	
 	socket.on('say', function(response){
-		console.log('Server said: ', response);
 		var theRoom = $('#socket-room');
 		if(theRoom[0].scrollHeight - theRoom.outerHeight() < theRoom.scrollTop())
 			var scrollDown = true;
 		
 		var username = response.text.split(':', 1)[0];
 		var message = response.text.substring(username.length + 1, response.text.length);
-		
-		theRoom.append('<p><span class="username">' + username + ':</span> <span class="'+ response.type +'">' + message+ '</span></p>');
+		var userType = username == $('#socket-users li:first').text() ? 'admin' : 'user';
+
+		theRoom.append('<p><span class="'+ userType +'">' + username + ':</span> <span class="'+ response.type +'">' + message+ '</span></p>');
 		
 		if(scrollDown) theRoom.scrollTop(theRoom[0].scrollHeight);
 	});
